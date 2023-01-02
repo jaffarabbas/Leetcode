5433f589b15a8af372e43ac7e1b9951b714998d0
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-02 10:19:00 +0500

    1-2-2022

---
+++ LeetCode/LeetCode/Questions/Roman to Integer.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+int RomanToInt(string s)
+{
+    Dictionary<char, int> roman = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+    int count = 0;
+    int prev = 0;
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        int value = roman[s[i]];
+        if(value >= prev)
+        {
+            count += value;
+        }
+        else
+        {
+            count -= value;
+        }
+        prev = value;
+    }
+    return count;
+}
+
+Console.WriteLine(RomanToInt("MCMXCIV"));
